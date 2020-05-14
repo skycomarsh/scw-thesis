39d994c8ede99180469055dc351d4a44e3f6e9c8
skycomarsh <56484135+skycomarsh@users.noreply.github.com>
2020-05-14 17:12:16 -0400

    added relative path to parsed_las

--- src/parsed_las.py
+++ src/parsed_las.py
@@ -2,8 +2,8 @@ import subprocess, os
 import laspy
 from las_extent import get_X_bounds, get_Y_bounds, get_X_length, get_Y_length, get_x_min, get_x_max, get_y_min, get_y_max
 
-#USER NEEDS TO INPUT THEIR ABSOLUTE PATH
-path_to_las = input(r"Absolute path to the LAS files")
+#USER NEEDS TO INPUT THEIR ABSOLUTE PATH TO THE .LAS FILES
+path_to_las = r"C:\Users\skyco\Documents\LAStests"
 dir_list = os.listdir(path_to_las)
 
 #iterating over the defined list
@@ -30,11 +30,12 @@ for file in range(len(dir_list)):
         for x in range(x_start, x_end, 30):
            
            #NEED TO INPUT USERS PATHS
-            os.system(r"C:/Users/skyco/Documents/GitHub/scw-thesis/LAStools/las2txt.exe" \
+            os.system(r"..\LAStools\las2txt.exe" \
             r" -i " + inputFile + \
-            r" -inside_tile {0} {1} 30 -drop_class 9 18 -o C:/Users/skyco/Documents/GitHub/scw-thesis/lasOutputData/{0}_{1}.txt" \
+            r" -inside_tile {0} {1} 30 -drop_class 9 18 -o ..\lasOutputData\{0}_{1}.txt" \
             r" -parse xyzc -sep comma" .format(x,y))
 
+print("COMPLETE")
 
 #FIGURE OUT HOW TO FIX
 #os.system(r"..\LAStools\las2txt.exe" \
